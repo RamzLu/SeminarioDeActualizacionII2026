f62437d6a740747dd2b4ea1ff3bfb1677dd02005
RamzLu <luanaabigail168@gmail.com>
2026-06-17 16:29:18 -0300

    arreglos en los trabajos

--- suma_multiplicacion_matrices/app.py
+++ suma_multiplicacion_matrices/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='public', static_folder='public', static_url_path='/')
 
 @app.route('/')
 def index():
